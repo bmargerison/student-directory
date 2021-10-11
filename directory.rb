16454669fdb3b38c744ed6db0a1a2ef26aeb1e44
bmargerison <bmargerison@outlook.com>
2021-10-11 10:50:07 +0100

    Refactoring arrays code

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 #let's put all the students into an array
 students = [
   "Dr. Hannibal Lecter",
@@ -16,16 +15,6 @@ students = [
 #and then print them
 puts "The students of Villains Academy"
 puts "------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each { |name| puts name }
 #finally, we print the total number of students
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"
